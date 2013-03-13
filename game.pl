3f8d331b3f9ca7d02209827a98d4e0bbe2e22e73
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-03-12 20:22:26 -0700

    first commit

---
+++ game.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+
+use constant THRESHOLD => 10000;
+
+my $stack = {
+    1 => 'red',
+    2 => 'red',
+    3 => 'red',
+    4 => 'blue',
+    5 => 'blue',
+    6 => 'blue',
+};
+
+sub draw {
+    my $stack = shift;
+    my @sides = keys %{$stack};
+    return $sides[ rand @sides ]; 
+}
+
+sub flip {
+    my $side = shift;
+    if ( $side % 2 == 0 ) {
+        return $side - 1;
+    } else {
+        return $side + 1;
+    }
+}
+
+my $count = {};
+do {
+    $face = draw( $stack );
+    $color = $stack->{$face};
+    $count->{$color}->{count}++;
+
+    if ( $color eq 'red' ) {
+        $face = flip( $face );
+        $color = $stack->{$face};        
+        $count->{red}->{$color}->{count}++;
+    }
+} while ( $count->{red}->{count} < THRESHOLD );
+
+printf ( "%f\n", $count->{red}->{red}->{count} / $count->{red}->{count} );
+
